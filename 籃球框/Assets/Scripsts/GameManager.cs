f7abe7b29c644614a1e5accf1c1cd894d1ccb329
1027046y <1027046y@gmail.com>
2020-05-29 12:42:57 +0800

    HWH_0508411_Final_VRVIVE

---
+++ 籃球框/Assets/Scripsts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("籃球數量")]
+    public Text textballcount;
+    
+    [Header("分數")]
+    public Text textscore;
+
+    private int ballcount = 5;
+    private int score;
+
+    private ThreePoint threePoint;
+
+    private void    Start()
+    {
+        threePoint = FindObjectOfType<ThreePoint>();
+    }
+
+    public void Useball(GameObject ball) 
+    {
+        Destroy(ball.GetComponent<Throwable>());
+        Destroy(ball.GetComponent<Interactable>());
+        ballcount--;
+        textballcount.text = "籃球數量:" + ballcount + "/5";
+    }
+
+    // Update is called once per frame
+    private void    OnTriggerEnter(Collider other)
+    {
+        if(threePoint.inThreePoint)
+        {
+            score += 3;
+        }
+        else
+        {
+            score += 2;
+        }
+        
+        textscore.text = "分數" + score;
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ 籃球框/Assets/Scripsts/ThreePoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ThreePoint : MonoBehaviour
+{
+    public bool inThreePoint;
+
+    private void OnTriggerEnter(Collider  other)
+    {
+        print(other.name);
+
+        if (other.name  == "HeadCollider")
+        {
+            inThreePoint = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "HeadCollider")
+        {
+            inThreePoint = false;
+        }
+    }
+}
